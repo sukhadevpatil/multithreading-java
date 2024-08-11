c8e8540dae4b7662085510352d691a93c24932d9
Sukhdev Patil <sukhadevpatil@gmail.com>
2024-08-11 23:03:41 +0530

    few more thread concepts

--- thread/src/io/section12/Main.java
+++ thread/src/io/section12/Main.java
@@ -1,2 +1,122 @@
-package io.section12;public class Main {
+package io.section12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        TicketReservationSystem reservationSystem = new TicketReservationSystem();
+        String trainName = "a";
+        int ticketCount = 50;
+
+        // These two threads are trying to book 50 tickets for train "a".
+        // After the operation expected available tickets are 0.
+        Thread t1 = new Thread(new TicketBookingTask(
+                reservationSystem,
+                trainName,
+                ticketCount));
+
+
+        Thread t2 = new Thread(new TicketBookingTask(
+                reservationSystem,
+                trainName,
+                ticketCount));
+
+        t1.start();
+        t2.start();
+
+        // Wait for both the threads to complete.
+        t1.join();
+        t2.join();
+
+        // Expected result is 0, but you might find 50.
+        System.out.println(
+                "Available tickets for train - " + trainName +
+                        " are " + reservationSystem.getAvailableTickets(trainName));
+
+    }
+}
+
+/*
+ * Class that is responsible for reserving/booking tickets.
+ */
+class TicketReservationSystem {
+
+    Map<String, Integer> reservationInfo = new HashMap<>();
+
+    public TicketReservationSystem() {
+        init();
+    }
+
+    private void init() {
+        reservationInfo.put("a", 100);
+        reservationInfo.put("b", 100);
+    }
+
+    /*
+     * Reserves the tickets for the given train,
+     * if there are sufficient tickets available.
+     */
+    public void reserveTickets(String trainName, int ticketCount) {
+        Integer available = reservationInfo.get(trainName);
+
+        if (available == null) {
+            System.out.println("Invalid train name " + trainName);
+            return;
+        }
+
+        if (available < ticketCount) {
+            System.out.println(
+                    "Could not reserve, requested seats not available");
+            return;
+        }
+
+        // For demo purpose.
+        // Intentionally passed on control to other thread
+        // before updating available tickets.
+        // This is to demonstrate that what if control is switched here..
+        Thread.yield();
+
+        // Reduce the available ticket count.
+        available -= ticketCount;
+
+        // Update the Map.
+        reservationInfo.put(trainName, available);
+    }
+
+    /*
+     * Returns available tickets associated with the given trainName.
+     */
+    public Integer getAvailableTickets(String trainName) {
+        return reservationInfo.get(trainName);
+    }
+}
+
+
+/*
+ * A Runnable task class that enables parallel ticket booking.
+ */
+class TicketBookingTask implements Runnable {
+
+    private TicketReservationSystem reservationSystem;
+    private String trainName;
+    private int ticketCount;
+
+    public TicketBookingTask(TicketReservationSystem reservationSystem,
+                             String trainName,
+                             int ticketCount) {
+        this.reservationSystem = reservationSystem;
+        this.trainName = trainName;
+        this.ticketCount = ticketCount;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            reservationSystem.reserveTickets(trainName, ticketCount);
+        }
+    }
+
 }
