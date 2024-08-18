3bb8a31b24c296552891755296583b53c2967c9c
Sukhdev Patil <sukhadevpatil@gmail.com>
2024-08-18 23:40:12 +0530

    first commit

--- thread/src/io/states/Lesson1.java
+++ thread/src/io/states/Lesson1.java
@@ -1,2 +1,19 @@
-package io.states;public class Lesson1 {
+package io.states;
+
+public class Lesson1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Main Thread :: " + Thread.currentThread().getState());
+
+        Runnable runnable = () -> {
+            System.out.println("  Hi there !!  ");
+        };
+
+        Thread thread = new Thread(runnable);
+        System.out.println("After thread creation :: " + thread.getState());
+        thread.start();
+        System.out.println("After thread start :: " + thread.getState());
+
+        thread.join();
+        System.out.println("After join :: " + thread.getState());
+    }
 }
